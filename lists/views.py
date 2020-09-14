8a49c3d2e8f6610eb7902f5deb08fe1ae185d39f
Guilherme Soares <guilherme.simoes@hotmart.com>
2020-09-13 23:07:41 -0300

    Redirecionando após o POST

--- lists/views.py
+++ lists/views.py
@@ -5,11 +5,9 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-  new_item_text = ''
   if request.method == 'POST':
     new_item_text = request.POST['item_text']
     Item.objects.create(text=new_item_text)
+    return redirect('/')
 
-  return render(request, 'home.html', {
-  'new_item_text': new_item_text
-  })
+  return render(request, 'home.html')
