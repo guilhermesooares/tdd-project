465715cdc6bb034ef40cabbbab9d1bbbeb50806a
Guilherme Soares <guilherme.simoes@hotmart.com>
2020-09-13 23:56:54 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 4

--- lists/tests.py
+++ lists/tests.py
@@ -13,19 +13,6 @@ class HomePageTest(TestCase):
   def test_home_page_returns_correct_html(self):
     response = self.client.get('/')  
     self.assertTemplateUsed(response, 'home.html')
-  
-  def test_can_save_a_POST_request(self):
-    self.client.post('/', data={'item_text': 'A new list item'})
-
-    self.assertEqual(Item.objects.count(), 1)
-    new_item = Item.objects.first()
-    self.assertEqual(new_item.text, 'A new list item')
-
-  def test_redirects_after_POST(self):
-    response = self.client.post('/', data={'item_text': 'A new list item'})
-
-    self.assertEqual(response.status_code, 302)
-    self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
   def test_only_saves_items_when_necessary(self):
     self.client.get('/')
@@ -55,8 +42,9 @@ class ListViewTest(TestCase):
     self.assertTemplateUsed(response, 'list.html')
 
   def test_displays_all_items(self):
-    Item.objects.create(text='itemey 1')
-    Item.objects.create(text='itemey 2')
+    my_list = List.objects.create()
+    Item.objects.create(text='itemey 1', list=my_list)
+    Item.objects.create(text='itemey 2', list=my_list)
 
     response = self.client.get('/lists/the-only-list-in-the-world/')
 
