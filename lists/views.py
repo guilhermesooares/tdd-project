f0dc8da842909efaafaa940235998df0f3b5fca4
Guilherme Soares <guilherme.simoes@hotmart.com>
2020-09-13 23:10:46 -0300

    python manage.py test

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from lists.models import Item
 
@@ -9,5 +9,4 @@ def home_page(request):
     new_item_text = request.POST['item_text']
     Item.objects.create(text=new_item_text)
     return redirect('/')
-
   return render(request, 'home.html')
