e08849a87dcf1e9fb25665f85de918909900790c
Guilherme Soares <guilherme.simoes@hotmart.com>
2020-10-18 19:30:18 -0300

    Entrada inválida não deve ser salva no B

--- lists/views.py
+++ lists/views.py
@@ -10,14 +10,16 @@ def home_page(request):
   return render(request, 'home.html')
 
 def new_list(request):
-    list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
-    try:
-        item.full_clean()
-    except ValidationError:
-        error = "You can't have an empty list item"
-        return render(request, 'home.html', {"error": error})
-    return redirect(f'/lists/{list_.id}/')
+  list_ = List.objects.create()
+  item = Item(text=request.POST['item_text'], list=list_)
+  try:
+    item.full_clean()
+    item.save()
+  except ValidationError:
+    list_.delete()
+    error = "You can't have an empty list item"
+    return render(request, 'home.html', {"error": error})
+  return redirect(f'/lists/{list_.id}/')
 
 def view_list(request, list_id):
   my_list = List.objects.get(id=list_id)
