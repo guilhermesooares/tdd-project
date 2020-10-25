34982169c91b23f61766af9bdab9682990fa17a5
Guilherme Soares <guilherme.simoes@hotmart.com>
2020-10-25 20:39:42 -0300

    Modelo de usuário mínimo

--- superlists/settings.py
+++ superlists/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lists'
+    'lists',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'accounts.User'
